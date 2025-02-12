e20c218c2b75b5941a1bc1561493b1d4c405663e
Karl Lew <karl@firepick.org>
2025-02-12 15:52:46 -0800

    v1.3.0 += tfidf

--- index.mjs
+++ index.mjs
@@ -3,7 +3,6 @@ import { FilePruner } from './src/memo-again/file-pruner.mjs';
 import { GuidStore } from './src/memo-again/guid-store.mjs';
 import { MemoCache } from './src/memo-again/memo-cache.mjs';
 import { Memoizer } from './src/memo-again/memoizer.mjs';
-
 export const MemoAgain = {
   Files,
   FilePruner,
